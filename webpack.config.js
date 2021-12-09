ddee944f8790c801b2bcc4aa974f541eee91e612
Aaron Faanes <dafrito@gmail.com>
2021-12-09 01:59:50 -0600

    Fix webpack lib name

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "parsegraph-checkglerror.js",
     globalObject: "this",
-    library: "parsegraph",
+    library: "parsegraph_checkglerror",
     libraryTarget: "umd",
   },
   module: {
